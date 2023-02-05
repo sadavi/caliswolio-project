fd65af5bb4ca4f28a05d4398459962c1ed1f4e73
Thomas Garland <ptgarland@protonmail.com>
2023-02-04 22:09:53 -0500

    Linked workoutapp.db to Django

--- Backend/caliswolio_backend/caliswolio_backend/settings.py
+++ Backend/caliswolio_backend/caliswolio_backend/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'caliswolio_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'workoutapp.sql',
     }
 }
 
