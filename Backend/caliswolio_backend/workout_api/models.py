7c56f7ce410873b597d94e2a3fa65e7daff9014b
Thomas Garland <ptgarland@protonmail.com>
2023-02-05 21:22:14 -0500

    Cleaned up the file

--- Backend/caliswolio_backend/workout_api/models.py
+++ Backend/caliswolio_backend/workout_api/models.py
@@ -1,8 +1,6 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
+# Hey so I was able to get Django to auto populate some of this but there are things we may want to manually to clean up:
 #   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` if it is needed
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
