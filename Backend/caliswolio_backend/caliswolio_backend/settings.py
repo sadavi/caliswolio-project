ea390528e7fba2b75f09b3425149d4ffaf1f0eb8
Thomas Garland <ptgarland@protonmail.com>
2023-02-05 20:53:52 -0500

    Gave Django permission to modify tables

--- Backend/caliswolio_backend/caliswolio_backend/settings.py
+++ Backend/caliswolio_backend/caliswolio_backend/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -68,14 +67,7 @@ TEMPLATES = [
     },
 ]
 
-# Rest Framework
-# https://www.django-rest-framework.org/
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
 
 WSGI_APPLICATION = 'caliswolio_backend.wsgi.application'
 
