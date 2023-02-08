b088e6be965f61bbdb29e53842ab88c1c668c6ea
Thomas Garland <ptgarland@protonmail.com>
2023-02-07 22:05:32 -0500

    Debugging migrations and setting up api serializer

--- Backend/caliswolio_backend/caliswolio_backend/urls.py
+++ Backend/caliswolio_backend/caliswolio_backend/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+# from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.models import User # maybe
-from rest_framework import routers, serializers, viewsets  # maybe
+from django.urls import include, path
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('workout_api.urls')),
 ]
--- Backend/caliswolio_backend/workout_api/admin.py
+++ Backend/caliswolio_backend/workout_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MemberAccount, FutureWorkout, PriorWorkout, PriorWorkoutExercises, TemplateExercises
 
-# Register your models here.
+admin.site.register(MemberAccount)
+admin.site.register(FutureWorkout)
+admin.site.register(PriorWorkout)
+admin.site.register(PriorWorkoutExercises)
+admin.site.register(TemplateExercises)
