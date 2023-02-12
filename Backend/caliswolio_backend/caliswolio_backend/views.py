aebe5c62e0ee62cba146cf6a1e74748866d366ec
mmears9795 <mmears9795@gmail.com>
2023-02-12 17:40:18 -0500

    Get Calls for Exercises, Levels, and MemberAccount

--- Backend/caliswolio_backend/caliswolio_backend/views.py
+++ Backend/caliswolio_backend/caliswolio_backend/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from workout_api.models import Level, Exercise
-from workout_api.serializers import ExerciseSerializer, LevelSerializer
+from workout_api.models import Level, Exercise, MemberAccount
+from workout_api.serializers import ExerciseSerializer, LevelSerializer, MemberAccountSerializer
 
 @api_view(['GET'])
 def getExercises(request):
@@ -15,3 +15,10 @@ def getLevels(request):
     levels = Level.objects.all()
     serializer = LevelSerializer(levels, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getMemberAccounts(request):
+    members = MemberAccount.objects.all()
+    serializer = MemberAccountSerializer(members, many=True)
+    return Response(serializer.data)
+
