e67353e70a8d36047b1c938f216f01a1859b5d7d
B0G0311 <ptgarland@protonmail.com>
2023-02-13 10:59:33 -0500

    views.py

--- Backend/caliswolio_backend/caliswolio_backend/views.py
+++ Backend/caliswolio_backend/caliswolio_backend/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from workout_api.models import FutureWorkout, Level, Exercise, MemberAccount
+from rest_framework import status
+from workout_api.models import *
 from workout_api.serializers import *
 
 # CREATE
-
+@api_view(['POST'])
+def createMemberAccount(request):
+    data = {
+        'Email': request.data.get('email'),
+        'Password': request.data.get('password'),
+        'Phone Number': request.data.get('phone_number'),
+        'Birth Year': request.data.get('birth_year'),
+        'Gender': request.data.get('gender'),
+        'Zipcode': request.data.get('zipcode'),
+        'Level': request.data.get('level_id'),
+    }
+    serializer =MemberAccountSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # READ
@@ -23,8 +39,14 @@ def getLevels(request):
 
 @api_view(['GET'])
 def getMemberAccounts(request):
-    members = MemberAccount.objects.all()
+    members = MemberAccounts.objects.all()
     serializer = MemberAccountSerializer(members, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getMemberAccount(request, member_id):
+    member = MemberAccounts.objects.get(id=member_id)
+    serializer = MemberAccountSerializer(member, many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
@@ -63,3 +85,38 @@ def getPriorWorkoutExercise(request):
     serializer = PriorWorkoutExerciseSerializer(pExercise, many=True)
     return Response(serializer.data)
 
+
+# UPDATE
+@api_view(['PUT'])
+def updateMemberAccount(request, member_id):
+    member_instance = getMemberAccount(request)
+    if not member_instance:
+        return Response(
+            {"res": "Object with member id does not exist"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    data = {
+        'Email': request.data.get('email'),
+        'Password': request.data.get('password'),
+        'Phone Number': request.data.get('phone_number'),
+        'Birth Year': request.data.get('birth_year'),
+        'Gender': request.data.get('gender'),
+        'Zipcode': request.data.get('zipcode'),
+        'Level': request.data.get('level_id'),
+    }
+    serializer = MemberAccountSerializer(instance=member_instance, data=data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# DELETE
+
+
+class MemberAccountView(APIView):
+    def get_object(self, member_id):
+        # Get the member account
+        try:
+            return MemberAccount.objects.get(id=member_id)
+        except MemberAccount.DoesNotExist:
+            return None
