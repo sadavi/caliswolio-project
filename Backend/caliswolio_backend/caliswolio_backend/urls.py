63275701bcb2ced83fa384311a4b4608f8cac41d
mmears9795 <mmears9795@gmail.com>
2023-02-14 18:24:53 -0500

    Commit before big change

--- Backend/caliswolio_backend/caliswolio_backend/urls.py
+++ Backend/caliswolio_backend/caliswolio_backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 # from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import include, path
+from caliswolio_backend import views
 
 
 
